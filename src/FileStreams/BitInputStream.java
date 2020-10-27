a13e109f454e423ee0729b488ea7b09baecc73c7
Данил Киреев <63447836+HolyGrease@users.noreply.github.com>
2020-10-27 18:38:43 +0300

    Update BitInputStream.java

--- src/FileStreams/BitInputStream.java
+++ src/FileStreams/BitInputStream.java
@@ -40,84 +40,38 @@ public class BitInputStream {
     }
 
     public boolean isEmpty(){
-        return endOfFile && current == left;
+
     }
 
     public boolean read() throws IOException {
-        return readFromBuffer();
+
     }
 
     public int read(int bits) throws IOException {
-        if (isEmpty())
-            return -1;
 
-        int number = 0;
-        for (int i = 0; i < bits; i++){
-            boolean bit = readFromBuffer();
-            number <<= 1;
-            if (bit) number |= 1;
-        }
-        return number;
     }
 
     public void close() throws IOException {
-        fileInputStream.close();
+
     }
 
     private void onEndOfFile(int read) {
-        endOfFile = true;
-        if (current == 0)
-            left = read - 1;
-        else
-            left = read - 1 + bufferSize - current;
-        bitsInLastByte = buffer[left];
 
-        for (int b: buffer)
-            System.out.print(b + " ");
-        System.out.println();
-        System.out.println(current);
-        System.out.println(left);
-        System.out.println(bitsInLastByte);
     }
 
     private boolean isByteRead(){
-        return n == 0;
+
     }
 
     private boolean isBufferFullEnough() {
-        return (bufferSize - current > 3) || endOfFile;
+
     }
 
     private void pushBuffer() throws IOException {
-        int read;
-        if (current == 0)
-            read = fileInputStream.read(buffer, current, bufferSize - current);
-        else{
-            System.arraycopy(buffer, current, buffer, 0, bufferSize - current);
-            read = fileInputStream.read(buffer, bufferSize - current, current);
-        }
 
-        if (fileInputStream.available() == 0)
-            onEndOfFile(read);
-
-        current = 0;
     }
 
     private boolean readFromBuffer() throws IOException {
-        if (!isBufferFullEnough())
-            pushBuffer();
 
-        n--;
-        boolean bit = ((buffer[current] >> n) & 1) == 1;
-
-        if (isByteRead()) {
-            n = byteSize;
-            current++;
-        }
-
-        if (current == left)
-            n = bitsInLastByte;
-
-        return bit;
     }
 }
